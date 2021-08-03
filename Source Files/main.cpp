ac83a14fa431667e830bd513edb116e8a8147a0f
TheKonamiKoder <66909206+TheKonamiKoder@users.noreply.github.com>
2021-08-03 14:26:27 +0100

    Updated main.cpp

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -1,10 +1,17 @@
 #include "QBoards.h"
 #include <QtWidgets/QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QBoards w;
+    QFile styleSheetF("StyleSheet.qss");
+    styleSheetF.open(QFile::ReadOnly);
+    QFont font("Courier New");
+    font.setStyleHint(QFont::Monospace);
+    qApp->setStyleSheet(styleSheetF.readAll());
+    qApp->setFont(font);
     w.show();
     return a.exec();
 }
